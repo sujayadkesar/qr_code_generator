f2219a75e29a149096953e1853644566aebdb541
SUJAY  ADKESAR <95465072+sujayadkesar@users.noreply.github.com>
2022-10-16 23:56:42 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,14 @@
-import qrcode
 import os
+
+
+os.system('sudo apt update -y')
+os.system('clear')
+os.system('sudo apt install python3-pip')
+os.system('pip3 install opencv-python')
+os.system('pip3 install pyfiglet')
+os.system('qrcode')
+os.system('clear')
+
 from PIL import Image
 os.system('pip install opencv-python')
 import cv2
