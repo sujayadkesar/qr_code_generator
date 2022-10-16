3c98317ea5a97bfa96cffe354f8364bfa82d7507
SUJAY  ADKESAR <95465072+sujayadkesar@users.noreply.github.com>
2022-10-16 23:46:02 +0530

    Update and rename requirements.txt to install_requirements.py

---
+++ install_requirements.py
@@ -0,0 +1,10 @@
+import os
+
+os.system('sudo apt update -y')
+os.system('clear')
+os.system('sudo apt install python3-pip')
+os.system('pip3 install opencv-python')
+os.system('pip3 install pyfiglet')
+os.system('qrcode')
+os.system('clear')
+print(' [*] All requiremets downloaded successfully .....')
